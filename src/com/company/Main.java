b967a78fc76634c32981afa053e375f5c48c11dd
Xenorus <ais191telegina@ya.ru>
2021-09-17 16:31:07 +0500

    6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,14 +13,37 @@ public class Main {
                 a = a + 2;
             }
         }
+
         long stop = System.nanoTime();
         String res1 = stopwatch(start, stop, 1);
+
+        a=0;
+        start = System.nanoTime();
+        for (int n=0; n<100000; n++) {
+            for (int i = 0; i < 900000; i++) {
+                a = a + 2;
+            }
+        }
+        stop = System.nanoTime();
+        String res2 = stopwatch(start, stop, 2);
+
+        a=0;
+        start = System.nanoTime();
+        for (int n=0; n<1000000; n++) {
+            for (int i = 0; i < 50000; i++) {
+                a = a + 2;
+            }
+        }
+        stop = System.nanoTime();
+        String res3 = stopwatch(start, stop, 3);
 
 
         //запись в файл
         try(FileWriter writer = new FileWriter("result.txt", false))
         {
             writer.write(res1);
+            writer.write(res2);
+            writer.write(res3);
             writer.flush();
         }
         catch(IOException ex){
@@ -29,6 +52,6 @@ public class Main {
     }
 
     public static String stopwatch (long start, long stop, int number) {
-        return ("Цикл " + number + " исполнялся " + ((stop - start)/1000000000) + " сек.");
+        return ("Цикл " + number + " исполнялся " + ((stop - start)/1000000000) + " сек. ");
     }
 }
