a8ba4918e084faec6edae8075a176831332ac68c
Xenorus <ais191telegina@ya.ru>
2021-09-17 12:19:03 +0500

    4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -11,10 +14,21 @@ public class Main {
             }
         }
         long stop = System.nanoTime();
-        stopwatch(start, stop, 1);
+        String res1 = stopwatch(start, stop, 1);
+
+
+        //запись в файл
+        try(FileWriter writer = new FileWriter("result.txt", false))
+        {
+            writer.write(res1);
+            writer.flush();
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
     }
 
-    public static void stopwatch (long start, long stop, int number) {
-        System.out.println("Программа " + number + "исполнялась" + ((stop - start)/1000000000) + " сек.");
+    public static String stopwatch (long start, long stop, int number) {
+        return ("Цикл " + number + "исполнялся" + ((stop - start)/1000000000) + " сек.");
     }
 }
